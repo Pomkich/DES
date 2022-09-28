9d9ad2d06876be9a8483c424d03dea524f17ec49
Роман Смирнов <pombl4.01@mail.ru>
2022-09-28 12:34:42 +0300

    implemented adding parity bits, added some comments

--- DES/main.cpp
+++ DES/main.cpp
@@ -7,35 +7,47 @@
 
 using namespace std;
 
-char bin_hex(std::string bits) {
-	static std::map<std::string, char> hex_bits = {	// for better perfomance static
-		{"0000", '0'}, {"0001", '1'}, {"0010", '2'}, {"0011", '3'},
-		{"0100", '4'}, {"0101", '5'}, {"0110", '6'}, {"0111", '7'},
-		{"1000", '8'}, {"1001", '9'}, {"1010", 'A'}, {"1011", 'B'},
-		{"1100", 'C'}, {"1101", 'D'}, {"1110", 'E'}, {"1111", 'F'},
-	};
-	return hex_bits[bits];
-}
-
-std::string byte_bin(unsigned char ch) {
-	std::string bits = "";
-	for (int i = 0; i < 8; i++) {
-		char bit = ((bool)(ch << i & 0b10000000) + '0');
-		bits.push_back(bit);
-	}
-	return bits;
-}
-
 unsigned char* permutate_P(unsigned char block[8]) {
-	unsigned char perm_block[8];
+	unsigned char* perm_block = new unsigned char[8];
 	memset(perm_block, 0, 8);
 	for (int i = 0; i < 64; i++) {
-		int offset_byte = (P[i / 16][i % 16] - 1) / 8;
-		int offset_bit = (P[i / 16][i % 16] - 1) % 8;
-		int bit = ((block[offset_byte] << offset_bit) & 0b10000000);
-		perm_block[i / 8] |= ((0b10000000 & bit) >> (i % 8));
+		int offset_byte = (P[i / 16][i % 16] - 1) / 8;	// смещение по байту
+		int offset_bit = (P[i / 16][i % 16] - 1) % 8;	// смещение по биту
+		int bit = ((block[offset_byte] << offset_bit) & 0b10000000);	// сохраняем бит
+		perm_block[i / 8] |= ((0b10000000 & bit) >> (i % 8));	// устанавливаем бит в текущую позицию
 	}
 	return perm_block;
+}
+
+int count_set_bits(unsigned char byte) {
+	int set = 0;
+	for (int i = 0; i < 8; i++) {
+		set += byte % 2;
+		byte /= 2;
+	}
+	return set;
+}
+
+unsigned char* add_key_bits(unsigned char key[7]) {
+	unsigned char* key_with_bits = new unsigned char[8];
+	memcpy(key_with_bits, key, 7);
+	memset(key_with_bits + 7, 0, 1);
+	for (int i = 0; i < 8; i++) {
+		char byte = key_with_bits[i] & 0b11111110;
+		int save_bit = key_with_bits[i] & 0b00000001;
+		if (count_set_bits(byte) % 2 != 0) {	// если число единиц нечётно ->
+			byte |= 0b00000001;					// добавляем до чётности
+		}
+		key_with_bits[i] = byte;	// сохраняем байт
+		// смещение остальных байтов
+		for (int j = i + 1; j < 8; j++) {
+			int temp_bit = key_with_bits[j] & 0b00000001;
+			key_with_bits[j] = key_with_bits[j] >> 1;
+			key_with_bits[j] |= (save_bit << 7);
+			save_bit = temp_bit;
+		}
+	}
+	return key_with_bits;
 }
 
 int main() {
@@ -44,7 +56,14 @@ int main() {
 		0b11111111, 0b11111111, 0b11111111, 0b11111111
 	};
 
+	unsigned char block2[8] = {
+		0b00000000, 0b00000000, 0b00000000, 0b00000000,
+		0b11111111, 0b11111111, 0b11111111
+	};
+
 	auto new_block = permutate_P(block);
+
+	auto bl = add_key_bits(block2);
 
 	return 0;
 }
@@ -107,3 +126,22 @@ std::ifstream infile("test.txt");
 	}
 
 */
+
+/*char bin_hex(std::string bits) {
+	static std::map<std::string, char> hex_bits = {	// for better perfomance static
+		{"0000", '0'}, {"0001", '1'}, {"0010", '2'}, {"0011", '3'},
+		{"0100", '4'}, {"0101", '5'}, {"0110", '6'}, {"0111", '7'},
+		{"1000", '8'}, {"1001", '9'}, {"1010", 'A'}, {"1011", 'B'},
+		{"1100", 'C'}, {"1101", 'D'}, {"1110", 'E'}, {"1111", 'F'},
+	};
+	return hex_bits[bits];
+}
+
+std::string byte_bin(unsigned char ch) {
+	std::string bits = "";
+	for (int i = 0; i < 8; i++) {
+		char bit = ((bool)(ch << i & 0b10000000) + '0');
+		bits.push_back(bit);
+	}
+	return bits;
+}*/
